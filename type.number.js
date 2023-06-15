e7680c3cb808c992c484c62915a0aa384882e170
devkarenmacedo <133161906+devkarenmacedo@users.noreply.github.com>
2023-06-15 18:54:03 -0300

    meus primeiros passos

---
+++ type.number.js
@@ -0,0 +1,8 @@
+const meuNumero =5;
+
+const primeiroNumero = 1;
+const SegundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero * SegundoNumero; 
+
+console.log(operacaoMatematica)
---
+++ type.string.js
@@ -0,0 +1,11 @@
+const text1 = "Ola, Mundo !";
+const text2 = "Ola, Mundo !";
+const senha = "senhaSuperSegura456!";
+const citacao = "meu nome é ";
+
+const meuNome = "Karen";
+
+
+//concatenação (+)
+
+console.log(citacao + meuNome)
